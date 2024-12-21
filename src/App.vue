87112faf4c264c18fd1feb3c7d5aefb6361179e8
WoolMike <emiliano.reyesca@gmai.com>
2024-12-20 20:38:02 -0600

    Usando el antd vue

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,26 @@ const useStore = useUserStore();
 </script>
 
 <template>
-  <nav v-if="!useStore.loadingSession">
-    <div>
-      <router-link to="/" class="btn btn-danger" v-if="useStore.userData">Home</router-link>
-      <router-link to="/login" class="btn btn-success" v-if="!useStore.userData">Login</router-link>
-      <router-link to="/register" class="btn btn-warning" v-if="!useStore.userData">Register</router-link>
-      <button class="btn btn-primary justify-content-md-end" @click="useStore.logoutUser"
-        v-if="useStore.userData">Logout</button>
+  <div>
+    <h1>App</h1>
+    <a-button type="primary">Boton ant</a-button>
+    <nav v-if="!useStore.loadingSession">
+      <div>
+        <router-link to="/" class="btn btn-danger" v-if="useStore.userData">Home</router-link>
+        <router-link to="/login" class="btn btn-success" v-if="!useStore.userData">Login</router-link>
+        <router-link to="/register" class="btn btn-warning" v-if="!useStore.userData">Register</router-link>
+        <button class="btn btn-primary justify-content-md-end" @click="useStore.logoutUser"
+          v-if="useStore.userData">Logout</button>
+      </div>
+    </nav>
+    <div v-else>
+      <h1>Cargando sesion...</h1>
     </div>
-  </nav>
-  <div v-else>
-    <h1>Cargando sesion...</h1>
-  </div>
-  <div class="container text-center">
-    <RouterView />
+    <div class="container text-center">
+      <RouterView />
 
+    </div>
   </div>
+
 
 </template>
